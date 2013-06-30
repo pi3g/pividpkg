d46996c1ce296477a50fa4adf81cdf4e77c03960
Lukas Senger <lukas@fridolin.com>
2013-06-30 15:44:31 +0000

    add pivid package

---
+++ pivid.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+git clone https://github.com/pi3g/pivid pividgit
+mkdir -p pivid/usr/share/pivid
+cp pividgit/playcommand.sh pividgit/node/* pivid/usr/share/pivid
+cd pivid/usr/share/pivid
+npm install express
+npm install jquery
+cd ../../..
+find usr -type f -printf 'usr/%P ' | xargs md5sum > DEBIAN/md5sums
+cd ..
+dpkg -b pivid pivid_0.0.1.deb
+rm -rf pividgit
+rm pivid/DEBIAN/md5sums
+rm -r pivid/usr/share
