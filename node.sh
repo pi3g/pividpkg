406084d281da51691df9892b95fc7e042c3d9e54
Lukas Senger <lukas@fridolin.com>
2013-06-30 15:00:40 +0000

    initial commit; node only

---
+++ node.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+cd node
+wget http://nodejs.org/dist/v0.10.12/node-v0.10.12-linux-arm-pi.tar.gz
+tar xf node-v0.10.12-linux-arm-pi.tar.gz
+rm node-v0.10.12-linux-arm-pi.tar.gz
+mv node-v0.10.12-linux-arm-pi usr
+rm usr/LICENSE usr/README.md usr/ChangeLog 
+find usr -type f -printf 'usr/%P ' | xargs md5sum > DEBIAN/md5sums
+cd ..
+dpkg -b node node_0.10.12.deb
+rm -r node/usr
+rm node/DEBIAN/md5sums
